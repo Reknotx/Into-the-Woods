9e53486895915e710fcdc9ee36e8ad77a5aaa545
JT Esmond <jtesmond.gaming@gmail.com>
2021-02-22 12:34:22 -0800

    MenuSystem w/out play buttons
    waiting on LDs to finish levels to set up play buttons

--- Assets/Scripts/UI Scripts/ClockUI.cs
+++ Assets/Scripts/UI Scripts/ClockUI.cs
@@ -4,173 +4,54 @@ using UnityEngine;
 using UnityEngine.UI;
 
 ///  Author: JT Esmond
-///  Date: 2/4/2021
+///  Date: 2/21/2021
 ///  <summary>
 /// The UI class that handles the physical representation of the day night cycle
 ///  </summary>
 
 public class ClockUI : MonoBehaviour
 {
-    /// <summary> Singleton instance of the CLock UI </summary>
-    public static ClockUI Instance;
+    //floats that are used in handling the Clock UI
+    public float timerMinutes;
+    private const float realSecondsPerInGameDay = 5f;
+    private float day;
 
-    /// <summary>
-    /// variables for the UI script
-    /// </summary>
-    private int limit;
+    //transform for the clockhand
+    private Transform clockHandTransform;
 
-    public void Awake()
+    //text reference for the clock UI text
+    private Text dayText;
+
+    private void Awake()
     {
-        if(Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-        }
-
-        Instance = this;
+        //setting the reference for the clock hand and the reference for the text element
+        clockHandTransform = transform.Find("clockHand");
+        dayText = transform.GetChild(0).transform.GetChild(0).GetComponent<Text>();
     }
 
-    private void Start()
+    private void Update()
     {
+        //sets the day float variable
+        day += Time.deltaTime / realSecondsPerInGameDay;
 
-    }
-    // Update is called once per frame
-    void Update()
-    {
-    }
+        //normalizes the day float
+        float dayNormalized = day % 1f;
 
-    //potential clock coroutine (not needed for now)
-    #region
-    /*
-    private IEnumerator timer()
-    {
-        sliderHolder.transform.position = new Vector3(-85, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-84.2f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-83.4f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-82.6f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-81.8f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-81, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-80.2f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-79.4f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-78.6f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-77.8f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-77, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-76.2f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-75.4f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-74.6f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-73.8f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-73, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-72.2f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-71.4f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-70.6f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-69.8f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-69, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-68.2f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-67.4f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-66.6f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-65.8f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-65, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-64.2f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-63.4f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-62.6f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-61.8f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-61, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-60.2f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-59.4f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-58.6f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-57.8f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-57, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-56.2f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-80.2f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-79.4f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-78.6f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-77.8f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-77, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-76.2f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-75.4f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-74.6f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-73.8f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-73, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-72.2f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-71.4f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-70.6f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-69.8f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-69, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-68.2f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-67.4f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-66.6f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-65.8f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-65, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-64.2f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-63.4f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-62.6f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-61.8f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-61, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-        sliderHolder.transform.position = new Vector3(-60.2f, 498.36f, 0);
-        yield return new WaitForSeconds(1f);
-    }*/
-    #endregion
+        //float for the amount of rotation per day
+        float rotationDegreesPerDay = 360f;
+
+        //rotates the clock hand the correct amount based off of the normalized day value
+        clockHandTransform.eulerAngles = new Vector3(0, 0, -dayNormalized * rotationDegreesPerDay);
+
+
+
+
+        //*** counter for the actual day counter, remove coments when day night cycle implemented
+        //string dayString = (Mathf.FloorToInt(day)).ToString("");
+        //dayText.text = ("Day: " + dayString);
+
+        //*** temporary timer info that can be deleted when the day night cycle is implemented
+        string minutesString = Mathf.Floor(timerMinutes - Mathf.Floor(day)).ToString("0");
+        dayText.text = minutesString + " Minutes";
+    }
 }
---
+++ Assets/Scripts/UI Scripts/MenuManagement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManagement : MonoBehaviour
+{
+
+    /// Author: JT Esmond
+    /// Date: 2/21/2021
+    /// <summary>
+    /// function for any Play Game button button.
+    /// </summary>
+    public void PlayGame()
+    {
+        //SceneManager.LoadScene();
+    }
+
+    /// Author: JT Esmond
+    /// Date: 2/21/2021
+    /// <summary>
+    /// Both of these are temporary functions to load the premade levels
+    /// </summary>
+    public void MapA()
+    {
+        SceneManager.LoadScene("MapA");
+    }
+    public void MapB()
+    {
+        SceneManager.LoadScene("MapB");
+    }
+
+
+    /// Author: JT Esmond
+    /// Date: 2/21/2021
+    /// <summary>
+    /// function for any How to play button. HowToPlay scene needs to be 1 in the build Index
+    /// </summary>
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    /// Author: JT Esmond
+    /// Date: 2/21/2021
+    /// <summary>
+    /// function for any main menu button. MainMenu scene needs to be 0 in the build Index
+    /// </summary>
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    /// Author: JT Esmond
+    /// Date: 2/21/2021
+    /// <summary>
+    /// function for any Shop button. Shop scene needs to be 3 in the build Index
+    /// </summary>
+    public void Shop()
+    {
+        //Opens shop screen
+        //SceneManager.LoadScene("Shop");
+    }
+
+    /// Author: JT Esmond
+    /// Date: 2/21/2021
+    /// <summary>
+    /// function for any Exit button.
+    /// </summary>
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
