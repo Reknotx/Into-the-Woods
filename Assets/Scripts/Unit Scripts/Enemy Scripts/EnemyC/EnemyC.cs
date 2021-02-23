5ad83b47a53f281a053a0b7073ae49889192f22d
Paul Hernandez <phernandez3@mail.csuchico.edu>
2021-02-22 17:35:53 -0800

    Chasey now does contact damage every second.

--- Assets/Scripts/Unit Scripts/Enemy Scripts/EnemyC/EnemyC.cs
+++ Assets/Scripts/Unit Scripts/Enemy Scripts/EnemyC/EnemyC.cs
@@ -9,6 +9,8 @@ using UnityEngine;
 
 public class EnemyC : Enemy
 {
+    protected bool onCooldown;
+
     ///Movement logic of Enemy C variants stays here
     ///
 
@@ -18,6 +20,29 @@ public class EnemyC : Enemy
 
         // All enemy C types chase the player.
         ChasePlayer(); 
+    }
+
+
+    protected void OnTriggerStay(Collider other)
+    {
+        if (!onCooldown && other.gameObject.layer == 8) // If "Player" layer.
+        {
+            Player.Instance.TakeDamage(1);
+            StartCoroutine(TackleCooldown(1f));
+            Debug.Log(onCooldown);
+        }
+    }
+
+    /// Author: Paul Hernandez
+    /// Date: 2/22/2021
+    /// <summary>
+    /// This puts the body contact hitbox for this unit on cooldown.
+    /// </summary>
+    protected IEnumerator TackleCooldown(float time)
+    {
+        onCooldown = true;
+        yield return new WaitForSeconds(time);
+        onCooldown = false;
     }
 
 }
