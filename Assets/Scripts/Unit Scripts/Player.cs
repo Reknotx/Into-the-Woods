a652b61f5a0dd74c144b412b07c671d3688d18d6
Reknotx <chaseoconnor3@comcast.net>
2021-03-04 13:44:03 -0800

    small changes made

--- Assets/Scripts/Unit Scripts/Player.cs
+++ Assets/Scripts/Unit Scripts/Player.cs
@@ -178,7 +178,7 @@ public class Player : Unit
         spellTicker.Tick(Time.deltaTime);
 
         /// The player wants to cast a spell.
-        if (Input.GetMouseButtonDown(0) && !BrewingSystem.Instance.gameObject.activeSelf) CastSpell();
+        if (Input.GetMouseButtonDown(0) && !PlayerInfo.SpellsFrozen && !BrewingSystem.Instance.gameObject.activeSelf) CastSpell();
         else if (Input.mouseScrollDelta.y > 0)
         {
             ///Move to next spell, spell 2 to spell 3
