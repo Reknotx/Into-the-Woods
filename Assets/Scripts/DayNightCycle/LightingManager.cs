c84cd240a9381bc72a41e6c9e7c5f14a4c1858e3
JT Esmond <jtesmond.gaming@gmail.com>
2021-03-08 14:06:34 -0800

    Comments for the day night cycle, and functionality for the popup windows

--- Assets/Scripts/DayNightCycle/LightingManager.cs
+++ Assets/Scripts/DayNightCycle/LightingManager.cs
@@ -7,24 +7,52 @@ using UnityEngine;
 /// <summary>
 /// Class that handles the functionality of the Day night cycle
 /// </summary>
+/// 
+[ExecuteAlways]
 public class LightingManager : MonoBehaviour
 {
-
+    //reference for the light in the scene
     [SerializeField] private Light DirectionalLight;
+
+    //reference for the preset lighting variables
     [SerializeField] private LightingPreset Preset;
 
+    //reference for the TimeOfDay variable;
     [SerializeField] private float TimeOfDay;
+
+    //variables used to check if its night or not
+    private float time;
+    private bool night;
+
 
     private void Update()
     {
+        time = transform.rotation.x;
+        if (time >= -25)
+        {
+            night = false;
+        }
+        else if (TimeOfDay >= 295 && TimeOfDay <= 60)
+        {
+            night = true;
+        }
+
         if (Preset == null)
             return;
+
+
         if (Application.isPlaying)
         {
             TimeOfDay += Time.deltaTime;
             TimeOfDay %= 360f;
+            UpdateLighting(TimeOfDay / 720f);
+        }
+        else
+        {
             UpdateLighting(TimeOfDay / 360f);
         }
+
+        Debug.Log(night);
 
     }
 
@@ -47,7 +75,7 @@ public class LightingManager : MonoBehaviour
         }
     }
 
-    
+    //turns on the Sun in the game view
     private void OnValidate()
     {
         if (DirectionalLight != null)
---
+++ Assets/Scripts/UI Scripts/PopUpManager.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// Author: JT Esmond
+/// Date: 3/8/2021
+/// <summary>
+/// class that holds the functions for the different pop up descriptions for items
+/// </summary>
+public class PopUpManager : MonoBehaviour
+{
+    public static PopUpManager Instance;
+
+    // Booleans for that are used to determine wether or not the different items have been picked up before
+    private bool totem;
+    private bool luckyPenny;
+    private bool attackCandy;
+    private bool balloonBouquet;
+    private bool nightOwl;
+    private bool twoPeas;
+    private bool compass;
+    private bool avocado;
+
+    public void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+    }
+
+    /// Author: JT Esmond
+    /// Date: 3/8/2021
+    /// <summary>
+    /// these functions control the different item pop ups. They pause the game time, turn on the specific pop up for the item
+    /// and sets the specific boolean to true so that pop up wont turn back on
+    /// </summary>
+    #region PopUp Functions
+    public void TotemPopUp()
+    {
+        if(!totem)
+        {
+            Time.timeScale = 0f;
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            totem = true;
+        }
+    }
+    public void LuckyPennyPopUp()
+    {
+        if (!luckyPenny)
+        {
+            Time.timeScale = 0f;
+            gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            luckyPenny = true;
+        }
+    }
+    public void AttackCandyPopUp()
+    {
+        if (!attackCandy)
+        {
+            Time.timeScale = 0f;
+            gameObject.transform.GetChild(2).gameObject.SetActive(true);
+            attackCandy = true;
+        }
+    }
+    public void BalloonBouquetPopUp()
+    {
+        if (!balloonBouquet)
+        {
+            Time.timeScale = 0f;
+            gameObject.transform.GetChild(3).gameObject.SetActive(true);
+            balloonBouquet = true;
+        }
+    }
+    public void NightOwlPopUp()
+    {
+        if (!nightOwl)
+        {
+            Time.timeScale = 0f;
+            gameObject.transform.GetChild(4).gameObject.SetActive(true);
+            nightOwl = true;
+        }
+    }
+    public void TwoPeasPopUp()
+    {
+        if (!twoPeas)
+        {
+            Time.timeScale = 0f;
+            gameObject.transform.GetChild(5).gameObject.SetActive(true);
+            twoPeas = true;
+        }
+    }
+    public void CompassPopUp()
+    {
+        if (!compass)
+        {
+            Time.timeScale = 0f;
+            gameObject.transform.GetChild(6).gameObject.SetActive(true);
+            compass = true;
+        }
+    }
+    public void AvocadoPopUp()
+    {
+        if (!avocado)
+        {
+            Time.timeScale = 0f;
+            gameObject.transform.GetChild(7).gameObject.SetActive(true);
+            avocado = true;
+        }
+    }
+    #endregion
+
+    /// Author: JT Esmond
+    /// Date: 3/8/2021
+    /// <summary>
+    /// functions for the different resume buttons.
+    /// </summary>
+    #region Resume Buttons
+    public void TotemResume()
+    {
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void LuckyPennyResume()
+    {
+        gameObject.transform.GetChild(1).gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void AttackCandyResume()
+    {
+        gameObject.transform.GetChild(2).gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void BalloonBouquetResume()
+    {
+        gameObject.transform.GetChild(3).gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void NightOwlResume()
+    {
+        gameObject.transform.GetChild(4).gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void TwoPeasResume()
+    {
+        gameObject.transform.GetChild(5).gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void CompassResume()
+    {
+        gameObject.transform.GetChild(6).gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void AvocadoResume()
+    {
+        gameObject.transform.GetChild(7).gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    #endregion
+}
