2669ca0c7a933906683a4b93580bca15e23ff871
Paul Hernandez <phernandez3@mail.csuchico.edu>
2021-03-10 18:36:31 -0800

    EnemyC_2 added

--- Assets/Scripts/Unit Scripts/Enemy Scripts/EnemyC/EnemyC_3.cs
+++ Assets/Scripts/Unit Scripts/Enemy Scripts/EnemyC/EnemyC_3.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyC_3 : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+/// <summary>
+/// Codename: "Twoey"
+/// "A high-level variant of this enemy will chase the player 
+/// and when they are destroyed the first time, 
+/// they will break into two enemies that must also be eliminated 
+/// before their complete death. 
+/// The original enemy will have 50 HP and when broken into two smaller enemies, 
+/// each of those will have 15 HP. When hitting the player, 
+/// the original enemy will deal 4 damage and the smaller enemies will deal 1 damage per hit."
+/// </summary>
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class EnemyC_3 : EnemyC
+{
+   
 }
