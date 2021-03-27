603abcd5e6e278d44942d27857cc0c502fd1901b
Paul Hernandez <phernandez3@mail.csuchico.edu>
2021-03-27 14:16:34 -0700

    Chase's suggestions

--- Assets/Scripts/World Scripts/WorldGenerator.cs
+++ Assets/Scripts/World Scripts/WorldGenerator.cs
@@ -3,49 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class WorldGenerator : MonoBehaviour
+public class WorldGenerator : SingletonPattern<WorldGenerator>
 {
-
     public int WorldRows; // Just the number of rows the world's rooms will be arranged in.
     public int WorldColumns; // Same for columns.
     public int minimumJourney; // The minimum distance between the home and boss tiles.
-    public float WorldXScale; // Scale of grid on the X axis. Currently bugged, minimumJourney doesn't play well.
-    public float WorldZScale; // Scale of grid on the Z axis. Currently bugged, minimumJourney doesn't play well.
+    public float WorldXScale; // Scale of grid on the X axis. Used for instantiating the game objects.
+    public float WorldZScale; // Scale of grid on the Z axis.
     private Transform WorldRoomsParent; // Empty object that will be used for organization in the hierarchy. All rooms parented to this!
 
     public bool manualSeed; // Do you want to force a certain seed?
     public int seed; // What exact randomization seed to use.
-    
-    public GameObject[] HomeRoomList; // The spawn room prefab. I call it home, because that's where the heart is.
-    public GameObject[] BossRoomList; // The boss room prefab.
-    public GameObject[] FieldRoomList; // List of field room prefabs to be assigned in the inspector.
 
-    [SerializeField] private List<GameObject> FieldRoomPicks = new List<GameObject>(); // The current list of picked field rooms for this world.
-    [SerializeField] private GameObject[] FieldRoomFinal; // The final array of picked field rooms as an array.
+    private List<GameObject> FieldRoomPicks = new List<GameObject>(); // The current list of picked field rooms for this world.
 
-    [SerializeField] private List<Vector3> gridPositions = new List<Vector3>(); // A list of possible locations to place tiles.
+    public WorldRoomContainer roomContainer; // The ScriptableObject that contains all of our room lists.
 
-    public GameObject[,] generatedMapArray; // the rooms we're using as a 2D array of objects.
+    public GameObject[,] roomArrange; // Our selected rooms to instantiate, in the arrangement we're building.
+    public GameObject[,] roomInstances; // The rooms we've instantiated as based FieldRoomPicks.
+    public Room[,] roomScripts; // The instances of roomInstances' scripts, "Room".
 
-    // These two have their own thing because they get re-rolled until they're a valid distance (minimumJourney) from each other.
-    private Vector3 intendedStart = new Vector3(); // The location of the starting home spawn room.
-    private Vector3 intendedEnd = new Vector3(); // The location of the ending boss room. 
-    private int intendedStartIndex; // Index of the above start location.
-    private int intendedEndIndex; // Index of the above end location.
-
-    public WorldRoomContainer roomContainer; //
-
-    // Start is called before the first frame update
-    void Start()
+    protected override void Awake()
     {
+        base.Awake();
+
         SafetyCheck();
+        // Ensures that variables aren't set to values that will cause errors.
 
         if (manualSeed)
             Random.InitState(seed);
 
         GenerateRoomList();
 
-        generatedMapArray = new GameObject[WorldColumns, WorldRows];
     }
 
     #region Setup
@@ -57,6 +46,10 @@ public class WorldGenerator : MonoBehaviour
     void SafetyCheck()
     {
         WorldRoomsParent = new GameObject("WorldRoomsParent").transform;
+
+        roomArrange = new GameObject[WorldColumns, WorldRows];
+        roomInstances = new GameObject[WorldColumns, WorldRows];
+        roomScripts = new Room[WorldColumns, WorldRows];
 
         if (WorldXScale == 0)
         {
@@ -85,7 +78,6 @@ public class WorldGenerator : MonoBehaviour
         }
 
     }
-
     #endregion
 
     #region MainWorldGeneration
@@ -98,230 +90,99 @@ public class WorldGenerator : MonoBehaviour
     /// </summary>
     public void GenerateRoomList()
     {
-        // Pick random field tiles to use.
-        FieldRoomPicks.Clear();
-        FieldRoomPicks.AddRange(FieldRoomList);
-        // I'm opting to add everything, then remove the excess, because we don't want duplicates from adding individually.
-        // Feel free to change if that's dumb. - Paul
+        // =========================================================================================
+        // First, pick random field tiles to use. ("Field" meaning not spawn or boss room.)
+        // These are stored in a list and will be put into a 2D array later for instancing.
+        // Previous iteration just took the entire existing list of field rooms, then
+        // randomly removed the excess until we had the desired number of rooms.
+        // Shuffle this list.
 
-        for (int i = 0; i < (FieldRoomList.Length - ((WorldRows * WorldColumns) - 2)); i++)
+        FieldRoomPicks.Clear();
+        FieldRoomPicks.AddRange(roomContainer.roomPrefabs);
+        for (int i = 0; i < (roomContainer.roomPrefabs.Count - ((WorldRows * WorldColumns) - 2)); i++)
         {
             // Remove entries from the list until we just have what we need to fill in the map, minus the spawn and boss rooms.
             FieldRoomPicks.RemoveAt(Random.Range(0, FieldRoomPicks.Count));
-        
+
         }
 
-        // Generate grid with coordinates
-        gridPositions.Clear();
-
-        // Loop through x axis (columns).
-        for (int x = 1; x < WorldColumns+1; x++)
+        // Shuffling the selected field rooms.
+        for (int i = 0; i < FieldRoomPicks.Count; i++)
         {
-            // Within each column, loop through z axis (rows).
-            for (int z = 1; z < WorldRows+1; z++)
-            {
-                // At each index add a new Vector3 to our list with the x and y coordinates of that position.
-                gridPositions.Add(new Vector3(x, 0f, z));
-
-                // Chase's suggested integration.
-                Vector3 pos = new Vector3(x * 20, 0f, z * 20);
-                GameObject temp = Instantiate(roomContainer.roomPrefabs[Random.Range(0,roomContainer.roomPrefabs.Count)], pos, Quaternion.identity);
-                generatedMapArray[x, z] = temp;
-            }
+            GameObject temp = FieldRoomPicks[i];
+            int randomIndex = Random.Range(i, FieldRoomPicks.Count);
+            FieldRoomPicks[i] = FieldRoomPicks[randomIndex];
+            FieldRoomPicks[randomIndex] = temp;
         }
 
-        // Find valid start and end points.
+
+        // =========================================================================================           
+        // Second, randomize two points in a 2D array and loop until they're a valid distance.
+        // We have safety checks in void Start so that we never have an infinite loop.
+        // We can do math on the 2D indexes to deduce if they're far enough away from each other,
+        // since the 2D indexes will reflect their placements asides from the physical distance.
+        // One of these will be the spawn, and the other will be the boss.
+
+        // Starting with two points.
+        Vector2Int intendedStart = new Vector2Int();
+        Vector2Int intendedEnd = new Vector2Int();
         do
         {
-            intendedStart = new Vector3();
-            intendedEnd = new Vector3();
-            SearchForValidSpace(HomeRoomList, "start");
-            SearchForValidSpace(BossRoomList, "end");
+            intendedStart = new Vector2Int(Random.Range(0, WorldColumns), Random.Range(0, WorldRows));
+            intendedEnd = new Vector2Int(Random.Range(0, WorldColumns), Random.Range(0, WorldRows));
 
-        } while ( ((Mathf.Abs(intendedStart.x - intendedEnd.x)) + (Mathf.Abs(intendedStart.z - intendedEnd.z)) ) < minimumJourney);
-        // While the absolute value of the sum of the two points' x's and z's is less than the minimum...
-        // In other words, keep rolling for different endpoints if they're too close together.
+        } while (((Mathf.Abs(intendedStart.x - intendedEnd.x)) + (Mathf.Abs(intendedStart.y - intendedEnd.y))) < minimumJourney);
 
-        // Instantiate them.
-        gridPositions.RemoveAt(intendedStartIndex);
-        if (intendedStartIndex < intendedEndIndex) 
+        // Committing to the array.
+        roomArrange[intendedStart.x, intendedStart.y] = roomContainer.spawnRoom;
+        roomArrange[intendedEnd.x, intendedEnd.y] = roomContainer.bossRoom;
+
+
+        // =========================================================================================
+        // Third, fill the empty spaces of the 2D array with the randomized field rooms from earlier.
+
+        List<GameObject> tempRoomList = FieldRoomPicks;
+
+        for (int i = 0; i < roomArrange.GetLength(0); i++)
         {
-            // We need to specifically have this because depending on where the two indexes are in relation
-            // to each other in the list, intendedEndIndex could get shifted downward, so we need to
-            // compensate for that case when it happens.
-            gridPositions.RemoveAt(intendedEndIndex-1);
-        }
-        else
-        {
-            gridPositions.RemoveAt(intendedEndIndex);
-        }
-        LayoutAtPoint(HomeRoomList, intendedStart);
-        LayoutAtPoint(BossRoomList, intendedEnd);
-
-
-        // Lay down the remainder of tiles.
-        FieldRoomFinal = FieldRoomPicks.ToArray();
-
-        //generatedMapArray = Make2DArray(FieldRoomFinal, WorldRows, WorldColumns);
-
-        LayoutAtRandom(FieldRoomFinal, (WorldRows*WorldColumns-2));
-
-    }
-
-
-    #endregion
-
-    #region FieldPlacement
-    /// Author: Paul Hernandez
-    /// Date: 2/4/2021
-    /// <summary>
-    /// Instantiates room object from a prefab list to a random location.
-    /// </summary>
-    /// <param name="tileArray">What set of room prefabs to use.</param>
-    /// <param name="objectCount">How many rooms are we placing?</param>
-    void LayoutAtRandom(GameObject[] tileArray, int objectCount)
-    {
-        //Instantiate objects until the limit objectCount is reached
-        for (int i = 0; i < objectCount; i++)
-        {
-
-
-            //Choose a position for randomPosition by getting a random position from our list of available Vector3s stored in gridPosition
-            Vector3 randomPosition = RandomPosition();
-
-
-            // Choose a random tile from tileArray and assign it to tileChoice.
-            // Pop off the one picked, so no duplicates.
-            List<GameObject> asdf = tileArray.ToList();
-            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
-            asdf.RemoveAt(System.Array.IndexOf(tileArray, tileChoice));
-            tileArray = asdf.ToArray();
-            // This is ugly, but I'm not sure how else to do this.
-            // I just needed to make sure that the list of available rooms doesn't yield duplicates.
-
-            //Instantiate tileChoice at the position returned by RandomPosition with no change in rotation.
-            GameObject instance = Instantiate(tileChoice, new Vector3(randomPosition.x * WorldXScale - WorldXScale, 0f, randomPosition.z * WorldZScale - WorldZScale), Quaternion.identity);
-            instance.transform.SetParent(WorldRoomsParent);
-        }
-    }
-
-    //RandomPosition returns a random position from our list gridPositions.
-    /// Author: Paul Hernandez
-    /// Date: 2/4/2021
-    /// <summary>
-    /// Returns a random position on the grid, and removes it from the list of available positions.
-    /// </summary>
-    /// <returns></returns>
-    Vector3 RandomPosition()
-    {
-        // Declare an integer randomIndex, set it's value to a random number between 0 and the count of items in our List gridPositions.
-        int randomIndex = Random.Range(0, gridPositions.Count -1); // -1 otherwise we can get an out of range index.
-
-        // Declare a variable of type Vector3 called randomPosition, set it's value to the entry at randomIndex from our List gridPositions.
-        Vector3 randomPosition = gridPositions[randomIndex];
-
-        // Remove the entry at randomIndex from the list so that it can't be re-used.
-        gridPositions.RemoveAt(randomIndex);
-
-        // Return the randomly selected Vector3 position.
-        return randomPosition;
-    }
-    #endregion
-
-    #region StartEndPointsPlacement 
-    /// Author: Paul Hernandez
-    /// Date: 2/4/2021
-    /// <summary>
-    /// Search for a single available position, and save it to either intendedStart or intendedEnd.
-    /// </summary>
-    /// <param name="tileArray">What array of room prefabs are we going to use?</param>
-    /// <param name="type">Are you searching for the "start" or "end" point?</param>
-    void SearchForValidSpace(GameObject[] tileArray, string type)
-    {
-        // Choose a position for randomPosition by getting a random position from our list of available Vector3s stored in gridPosition
-        Vector3 randomPosition = RandomPositionSearch(type);
-
-        // Choose a random tile from tileArray and assign it to tileChoice
-        GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
-
-        // 
-        if (type == "start")
-        {
-            intendedStart = randomPosition;
-        }
-        else if (type == "end")
-        {
-            intendedEnd = randomPosition;
-        }
-
-    }
-
-    /// Author: Paul Hernandez
-    /// Date: 2/4/2021
-    /// <summary>
-    /// Search for a random position, and save its index to either intendedStartIndex or intendedEndIndex.
-    /// </summary>
-    /// <param name="type">Are you searching for the "start" or "end" point?</param>
-    /// <returns></returns>
-    Vector3 RandomPositionSearch(string type)
-    {
-        //Declare an integer randomIndex, set it's value to a random number between 0 and the count of items in our List gridPositions.
-        int randomIndex = Random.Range(0, gridPositions.Count-1); // -1 otherwise we can get an out of range index.
-
-        //Declare a variable of type Vector3 called randomPosition, set it's value to the entry at randomIndex from our List gridPositions.
-        Vector3 randomPosition = gridPositions[randomIndex];
-
-        // 
-        if (type == "start")
-        {
-            intendedStartIndex = randomIndex;
-        }
-        else if (type == "end")
-        {
-            intendedEndIndex = randomIndex;
-        }
-
-        //Return the randomly selected Vector3 position.
-        return randomPosition;
-    }
-
-    /// Author: Paul Hernandez
-    /// Date: 2/4/2021
-    /// <summary>
-    /// Place a specific tile at a specific point.
-    /// </summary>
-    /// <param name="tileArray">What array of room prefabs are we going to use?</param>
-    /// <param name="tileType">Are you laying down the "start" or "end point?</param>
-    void LayoutAtPoint(GameObject[] tileArray, Vector3 tileType)
-    {
-        //Choose a random tile from tileArray and assign it to tileChoice
-
-        ///Paul subtracting by -1 here on the length is actually incorrect as the way
-        ///Random.Range() works if you hover over it is it gets a number between min and
-        ///max with max being exclusive meaning that if you have a range of 0 - 10 you 
-        ///will get a number back between 0 and 9 instead. 
-        GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
-
-        //Instantiate tileChoice at the position returned by RandomPosition with no change in rotation.
-        GameObject instance = Instantiate(tileChoice, new Vector3(tileType.x * WorldXScale - WorldXScale, 0f, tileType.z * WorldZScale - WorldZScale), Quaternion.identity);
-        instance.transform.SetParent(WorldRoomsParent);
-
-    }
-    #endregion
-
-    #region ArrayTransferNonsense
-    private static T[,] Make2DArray<T>(T[] input, int height, int width)
-    {
-        T[,] output = new T[height, width];
-        for (int i = 0; i < height; i++)
-        {
-            for (int j = 0; j < width; j++)
+            for (int z = 0; z < roomArrange.GetLength(1); z++)
             {
-                output[i, j] = input[i * width + j];
+                if (roomArrange[i, z] != null)
+                {
+                    //print(i +"," + z + " was not null; " + roomArrange[i,z]);
+                    // Do nothing?
+                }
+                else
+                {
+                    //print(i + "," + z + " was null.");
+
+                    int randomRoom = Random.Range(0, tempRoomList.Count);
+                    roomArrange[i, z] = tempRoomList[randomRoom];
+                    tempRoomList.RemoveAt(randomRoom);
+
+                    //print(roomArrange[i, z]);
+                }
             }
         }
-        return output;
+
+
+        // =========================================================================================
+        // Fourth, instantiate the 2D array of rooms.
+        // Use a second 2D array to refer to the instances.
+
+        for (int i = 0; i < roomArrange.GetLength(0); i++)
+        {
+            for (int z = 0; z < roomArrange.GetLength(1); z++)
+            {
+                // I feel like I've made a redundant amount of room arrays...?
+                roomInstances[i, z] = Instantiate(roomArrange[i, z], new Vector3(i * WorldXScale, 0f, z * WorldZScale), Quaternion.identity);
+                roomInstances[i, z].transform.SetParent(WorldRoomsParent);
+                roomScripts[i, z] = roomInstances[i, z].GetComponent<Room>();
+            }
+        }
+
     }
     #endregion
+
 
 }
