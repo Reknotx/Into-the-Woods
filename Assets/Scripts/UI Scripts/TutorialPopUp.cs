1a38fb366a2bb4b85f3c6c2204d7e3fedb3ecce8
Paul Hernandez <phernandez3@mail.csuchico.edu>
2021-04-11 20:04:19 -0700

    Tutorial UI text from Olivia

--- Assets/Scripts/UI Scripts/TutorialPopUp.cs
+++ Assets/Scripts/UI Scripts/TutorialPopUp.cs
@@ -6,10 +6,11 @@ using UnityEngine.UI;
 public class TutorialPopUp : MonoBehaviour
 {
     private int currentPage = 0;
-    private int maxPages = 2;
+    private int maxPages = 5; // Add 1 mentally, since we start at 0.
 
     public GameObject TutorialUI;
-    public TMPro.TMP_Text description;
+    public TMPro.TMP_Text blurb;
+    public TMPro.TMP_Text details;
     public TMPro.TMP_Text UIPageNumber;
     public RawImage videoImage;
 
@@ -52,21 +53,33 @@ public class TutorialPopUp : MonoBehaviour
     private void RefreshUIChoice()
     {
         UIPageNumber.text = ((currentPage + 1) + "/" + (maxPages + 1));
+        blurb.text = "";
+        details.text = "";
 
-        switch (currentPage)
+        switch (currentPage + 1)
         {
-            case 0:
-                description.text = "This is the first page!";
-                break;
             case 1:
-                description.text = "This is the second page!";
+                blurb.text = "Move around the map using WASD.";
                 break;
             case 2:
-                description.text = "This is the third page!";
+                blurb.text = "Use LMB to cast spells.";
+                details.text = "Switch spells using the scroll wheel on your mouse or Q and E on your keyboard. Look out for additional spells as you are exploring the forest!";
                 break;
             case 3:
+                blurb.text = "Find a cauldron to brew potions!";
+                details.text = "You will need to collect ingredients in the forest as you explore, but you have one on you when you start. Potions will be sent to your hotbar and can be consumed using 1, 2, and 3.";
                 break;
             case 4:
+                blurb.text = "It’s fun to explore at night!";
+                details.text = "...but be careful! Keep an eye out for doors that might be closed during the day.";
+                break;
+            case 5:
+                blurb.text = "Find your master and free him!";
+                details.text = "Travel from room to room and defeat enemies along the way.";
+                break;
+            case 6:
+                blurb.text = "Time is of the essence!";
+                details.text = "You only have three days to find and free your master before he is gone forever... but beware. You’ll have to defeat his captor first!";
                 break;
             default:
                 print("Default Tutorial case. You shouldn't be seeing this.");
